572aa88540bd506d38ce93f3d1ccb1d8457434bb
gmawji <git@gmawji.com>
2017-05-12 05:52:36 -0400

    jsbeautifier

--- js/engine.js
+++ js/engine.js
@@ -13,7 +13,6 @@
  * the canvas' context (ctx) object globally available to make writing app.js
  * a little simpler to work with.
  */
-
 var Engine = (function(global) {
     /* Predefine the variables we'll be using within this scope,
      * create the canvas element, grab the 2D context for that canvas
@@ -110,12 +109,12 @@ var Engine = (function(global) {
          * for that particular row of the game level.
          */
         var rowImages = [
-                'images/water-block.png',   // Top row is water
-                'images/stone-block.png',   // Row 1 of 3 of stone
-                'images/stone-block.png',   // Row 2 of 3 of stone
-                'images/stone-block.png',   // Row 3 of 3 of stone
-                'images/grass-block.png',   // Row 1 of 2 of grass
-                'images/grass-block.png'    // Row 2 of 2 of grass
+                'images/water-block.png', // Top row is water
+                'images/stone-block.png', // Row 1 of 3 of stone
+                'images/stone-block.png', // Row 2 of 3 of stone
+                'images/stone-block.png', // Row 3 of 3 of stone
+                'images/grass-block.png', // Row 1 of 2 of grass
+                'images/grass-block.png' // Row 2 of 2 of grass
             ],
             numRows = 6,
             numCols = 5,
@@ -134,7 +133,8 @@ var Engine = (function(global) {
                  * so that we get the benefits of caching these images, since
                  * we're using them over and over.
                  */
-                ctx.drawImage(Resources.get(rowImages[row]), col * 101, row * 83);
+                ctx.drawImage(Resources.get(rowImages[row]), col * 101,
+                    row * 83);
             }
         }
 
@@ -151,13 +151,13 @@ var Engine = (function(global) {
          */
 
         if (renderGame === true) {
-        allEnemies.forEach(function(enemy) {
-            enemy.render();
-        });
+            allEnemies.forEach(function(enemy) {
+                enemy.render();
+            });
 
-        player.render();
-        gem.render();
-        star.render();
+            player.render();
+            gem.render();
+            star.render();
         }
     }
 
